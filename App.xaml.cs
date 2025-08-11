c0ad64e58b907bc663433fb1a2faeca92d278653
LukeJohnson03 <lukejohnsonlgj905@gmail.com>
2025-08-11 15:26:50 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CuttingOptimiserDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
